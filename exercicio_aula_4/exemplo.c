ef40632290042aaf2459328c6b9cfaa9dcce11d7
Stefane <stefane.rodriguesufpa@gmail.com>
2023-04-29 20:20:47 -0300

    atualizado

---
+++ exercicio_aula_4/exemplo.c
@@ -0,0 +1,17 @@
+#include <stdio.h> //bliblioteca
+
+
+//a proxima linha representa o bloco de incio
+
+
+int main() {
+   int n1, n2, r;
+
+
+   printf("Digite N1: ");
+   scanf("%d", &n1);
+   printf("Digite N2: ");
+   scanf("%d", &n2);
+   r = n1 + n2;
+   printf("a soma eh %d", r);
+}
