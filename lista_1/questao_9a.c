edf8465b0cf5707220297e698966bb436f74c891
Stefane13th <stefane.rodriguesufpa@gmail.com>
2023-04-30 20:08:11 -0300

    atualizar questão 9

--- lista_1/questao_9a.c
+++ lista_1/questao_9a.c
@@ -2,5 +2,17 @@
  a. Ler	a temperatura	em	graus	Celsius	e apresentá-la	convertida	em	Fahrenheit.  
  A fórmula de conversão é: Fahrenheit = ( Celsius*1,8)*/
 
-include <stdio.h>
+#include <stdio.h>
 
+int main () {
+
+    float cel = 0, fah = 0;
+
+    printf("Digite a temperatura em graus Celsius:");
+    scanf("%f",&cel);
+
+    fah = cel*1.8;
+
+    printf("Mostre o valor convertido %f", fah);
+    scanf("%f",&fah);
+}
