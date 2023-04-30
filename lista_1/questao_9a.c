b231c969ad95b86b8395076fc800ae6aec956c8a
Stefane13th <stefane.rodriguesufpa@gmail.com>
2023-04-30 19:55:41 -0300

    alteração

---
+++ lista_1/questao_9a.c
@@ -0,0 +1,6 @@
+/* Desenvolva o  algoritmo e a codificação para os  seguintes programas (português estruturado ou  C):
+ a. Ler	a temperatura	em	graus	Celsius	e apresentá-la	convertida	em	Fahrenheit.  
+ A fórmula de conversão é: Fahrenheit = ( Celsius*1,8)*/
+
+include <stdio.h>
+
