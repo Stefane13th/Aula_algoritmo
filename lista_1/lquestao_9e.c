543c251dfd2933d4c9f7a136151fd1f0bb66638f
Stefane13th <stefane.rodriguesufpa@gmail.com>
2023-05-01 18:01:08 -0300

    atualização das listas

---
+++ lista_1/lquestao_9e.c
@@ -0,0 +1,7 @@
+/*Ler	quatro	valores	numéricos inteiros	e	apresentar	o	resultado	das	adições	e	
+das	 multiplicações	 utilizando	 a propriedade	 distributiva	 para	 a	 máxima	
+combinação	possível entre	as	quatro	variáveis.	Por	exemplo, considerando	o	
+uso	 das variáveis	A,  B, C e D, devem	 ser feitas	 seis adições e	 seis	
+multiplicações,	ou	seja, deve	ser	combinada	a	variável	A	com	B,	A	com	C	e	A	
+com	D.	Depois	é necessário combinar	a	variável B	com	a	C	e	a	B	com	a	D.	Por	
+último	combinar C	com	D.*/
