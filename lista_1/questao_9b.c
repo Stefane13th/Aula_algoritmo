de5cf5a7651f833a3ee7ef36260854beef4c9a81
Stefane13th <stefane.rodriguesufpa@gmail.com>
2023-04-30 22:40:14 -0300

    atualizar

--- lista_1/questao_9b.c
+++ lista_1/questao_9b.c
@@ -1,16 +1,19 @@
 /*Calcular	 o	 volume	 de	 uma	 lata	 de	 óleo	 utilizando	 a	 fórmula:	 volume	 =	
 3.14159	x	r2	x	Altura*/
 
+#include <stdio.h> //biblioteca
+#include <math.h>
+
 int main() {
     float a = 0, v = 0, r = 0;
 
 //Recebendo o valor do raio da lata na variável "r"
-    printf("Digite o valor raio",r);
-    scanf("%f",&r);
+    printf("Digite o valor raio:",r);
+    scanf("%2.f",&r);
 
 //Recebendo o valo da altura da lata na variável "a"
-    printf("Digite a altura da lata");
-    scanf("%f",a);
+    printf("Digite a altura da lata:");
+    scanf("%2.f",a);
 
 //Calculo do volume da lata de óleo
     v = 3.14159*pow(r,2)*a;
