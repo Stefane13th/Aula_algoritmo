e1f2cac7394f0671a20a491df99039d4b7a113a9
Stefane13th <stefane.rodriguesufpa@gmail.com>
2023-04-30 22:47:42 -0300

    questão 9b

--- lista_1/questao_9b.c
+++ lista_1/questao_9b.c
@@ -8,12 +8,12 @@ int main() {
     float a = 0, v = 0, r = 0;
 
 //Recebendo o valor do raio da lata na variável "r"
-    printf("Digite o valor raio:",r);
+    printf("Digite o valor raio:");
     scanf("%2.f",&r);
 
 //Recebendo o valo da altura da lata na variável "a"
     printf("Digite a altura da lata:");
-    scanf("%2.f",a);
+    scanf("%2.f",&a);
 
 //Calculo do volume da lata de óleo
     v = 3.14159*pow(r,2)*a;
