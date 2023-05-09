508525f3e0962b49012470d88cb1027758ce88b9
Stefane13th <stefane.rodriguesufpa@gmail.com>
2023-05-09 14:44:24 -0300

    add questão 1 vetores

--- lista_4_vetores/questao_1.c
+++ lista_4_vetores/questao_1.c
@@ -5,13 +5,21 @@ números ímpares. Apresente como resultado as quantidades encontradas.*/
 include <stdio.h>
 
 int main () {
-    int vet[10], i;
+    int vet[10], i, y, y2;
 
     printf("Digite 10 valor para armazenar no vetor: %d");
     
     for (i = 0; i < 10; i++) {
         printf("Digite o valor: %d");
-        scanf("%d",vetor[i]);
+        scanf("%d",vet[i]);
         printf("\n");
     }
+    for (i = 0; i < 10; i++) {
+        if (vet[i] % 2 == 0) {
+            y++;
+        } else {
+            y2++;
+        }
+    }
+    printf("A quantidade de pares são %d e quantidade de impares são %d",y,y2);
 }
