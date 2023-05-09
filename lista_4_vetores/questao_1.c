fbc0bd83c9459abfddaf691d41a1e641e3aeabd1
Stefane13th <stefane.rodriguesufpa@gmail.com>
2023-05-09 14:17:42 -0300

    lista_4_vetores

---
+++ lista_4_vetores/questao_1.c
@@ -0,0 +1,17 @@
+/*Ler 10 valores inteiros e os armazene em um vetor. O programa deve
+percorrer o vetor e contar a quantidade de números pares e a quantidade de
+números ímpares. Apresente como resultado as quantidades encontradas.*/
+
+include <stdio.h>
+
+int main () {
+    int vet[10], i;
+
+    printf("Digite 10 valor para armazenar no vetor: %d");
+    
+    for (i = 0; i < 10; i++) {
+        printf("Digite o valor: %d");
+        scanf("%d",vetor[i]);
+        printf("\n");
+    }
+}
